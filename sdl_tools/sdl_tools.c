924c12e3bf8db23441d39c9170b37f438dc362bc
william grolleau <william.grolleau@epita.fr>
2019-10-06 05:44:59 +0200

    Add getpixel function

--- sdl_tools/sdl_tools.c
+++ sdl_tools/sdl_tools.c
@@ -4,6 +4,7 @@
 
 #include "sdl_tools.h"
 #include <stdint.h>
+#include <SDL/SDL_image.h>
 
 static inline Uint8* pixelref(SDL_Surface * InputImage, int x, int y)
 {
@@ -33,5 +34,40 @@ Uint32 getpixel(SDL_Surface* InputImage, int x, int y)
 
 	default:
 		return 0; //Should not happen, but avoids warnings
+	}
+}
+
+void putpixel(SDL_Surface* InputImage, int x, int y, Uint32 pixel)
+{
+	Uint8* p = pixelref(InputImage, x, y);
+
+	switch(InputImage->format->BytesPerPixel)
+	{
+	case 1:
+		*p = pixel;
+		break;
+
+	case 2:
+		*(Uint16 *)p = pixel;
+		break;
+
+	case 3:
+		if(SDL_BYTEORDER == SDL_BIG_ENDIAN)
+		{
+			p[0] = (pixel >> 16) & 0xff;
+			p[1] = (pixel >> 8) & 0xff;
+			p[2] = pixel & 0xff;
+		}
+		else
+		{
+			p[0] = pixel & 0xff;
+			p[1] = (pixel >> 8) & 0xff;
+			p[2] = (pixel >> 16) & 0xff;
+		}
+		break;
+
+	case 4:
+		*(Uint32 *)p = pixel;
+		break;
 	}
 }
--- sdl_tools/sdl_tools.h
+++ sdl_tools/sdl_tools.h
@@ -2,7 +2,7 @@
 #define SDL_TOOLS
 
 #include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
+//#include <SDL/SDL_image.h>
 
 Uint32 getpixel(SDL_Surface* InputImage, int x, int y);
 void putpixel(SDL_Surface* InputImage, int x, int y, Uint32 pixel);
