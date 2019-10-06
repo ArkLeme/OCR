9552cbaca7ebb7907b040a3e841b7d50efbaee47
william grolleau <william.grolleau@epita.fr>
2019-10-06 13:44:25 +0200

    Add historigramm

--- preprocessing/preprocessing.c
+++ preprocessing/preprocessing.c
@@ -43,28 +43,32 @@ SDL_Surface* Contrast(SDL_Surface* InputImage)
 
 	int w = InputImage -> w, h = InputImage -> h, sum = 0;
 	int histogram[256], newValue[256];
-	float multiplicator = 1 / (w * h);
+	float multiplicator = (float) 255 / (w * h);
 
 	SDL_PixelFormat* format = InputImage -> format;
 	SDL_Surface* OutputImage = CopySurface(InputImage);
 
+	//Set array to 0 
 	for(int i = 0; i < 256; i++)
+	{
 		histogram[i] = 0;
-
+		newValue[i] = 0;
+	}
 	for(int i = 0; i < w; i++)
 	{
 		for(int j = 0; j < h; j++)
 		{
 			pixel = GetPixel(InputImage, i, j);
 			SDL_GetRGB(pixel, format, &r, &g, &b);
+
 			histogram[(int) r]++;
 		}
 	}
 
 	for(int i = 0; i < 256; i++)
 	{
-	sum += histogram[i];
-	newValue[i] = (int) (sum * multiplicator);
+		sum += histogram[i];
+		newValue[i] = (int) (sum * multiplicator);
 	}
 
 	for(int i = 0; i < w; i++)
