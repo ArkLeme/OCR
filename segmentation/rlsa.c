2fa12b0cb44dbddbd085daab6183277ec365c00a
william grolleau <william.grolleau@epita.fr>
2019-10-13 12:20:52 +0200

    Debug Image to matrix and matrix to image

--- segmentation/rlsa.c
+++ segmentation/rlsa.c
@@ -1,22 +1,40 @@
 #include "rlsa.h"
-#include "../sdl_tools/sdl_tools.h"
 
-void SaveMatAsIm(Matrix m)
+void SaveMatAsIm(Matrix m, char* path)
 {
 	SDL_Surface* temp = CreateSurface(m.col, m.line);
 	Uint32 pixel;
 	Uint8 r;
 
-	for(int i = 0; i < m.col; i++)
+	for(int i = 0; i < m.line; i++)
 	{
-		for(int j = 0; j < m.line; j++)
+		for(int j = 0; j < m.col; j++)
 		{
 			r = GetM(m, i, j) ? 0 : 255;
 			pixel = SDL_MapRGB(temp -> format, r, r, r); 
-			PutPixel(temp, i, j, pixel);
+			PutPixel(temp, j, i, pixel);
 		}
 	}
 
-	SaveImage(temp, "image_data/testMat.bmp");
+	SaveImage(temp, path);
 }
 
+
+Matrix GetMatFromIm(SDL_Surface* InputImage)
+{
+	Matrix temp = InitM(InputImage -> h, InputImage -> w);
+	Uint8 r;
+	Uint32 pixel;
+
+	for(int i = 0; i < temp.line; i++)
+	{
+		for(int j = 0; j < temp.col; j++)
+		{
+			pixel = GetPixel(InputImage, j, i);
+			SDL_GetRGB(pixel, InputImage -> format, &r, &r, &r);
+			PutM(temp, i, j, r == 0);
+		}
+	}
+
+	return temp;
+}
--- segmentation/rlsa.h
+++ segmentation/rlsa.h
@@ -1,7 +1,11 @@
 #ifndef RLSA
 #define RLSA
 #include "../matrix/matrix.h"
+#include "../sdl_tools/sdl_tools.h"
 
-void SaveMatAsIm(Matrix m);
+//Save matrix as image
+void SaveMatAsIm(Matrix m, char* path);
 
+//Load matrix from image
+Matrix GetMatFromIm(SDL_Surface* InputImag);
 #endif
