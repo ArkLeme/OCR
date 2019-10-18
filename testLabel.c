1c5ea92b9239c8fb03953d65730e2fb0349b390c
william.Grolleau <william.grolleau@epita.fr>
2019-10-18 11:21:37 +0200

    Swap pc bug ?

---
+++ testLabel.c
@@ -0,0 +1,65 @@
+#include <err.h>
+#include "sdl_tools/sdl_tools.h"
+#include "preprocessing/preprocessing.h"
+#include "matrix/matrix.h"
+
+void testBin(char* path);
+void testM();
+
+int main(int argc, char** argv)
+{
+	if(argc == 2)
+		testBin(argv[1]);
+
+	//testM();
+
+	return 0;
+}
+
+void testBin(char* path)
+{
+	SDL_Surface* InputImage = LoadImage(path);
+
+	SDL_Surface* GrayImage = GrayScale(InputImage);
+	SDL_Surface* ContrastImage = Contrast(InputImage);
+	SDL_Surface* BinImage = Otsu(ContrastImage);
+
+	SaveImage(GrayImage, "image_data/GrayImage.bmp");
+	SaveImage(ContrastImage, "image_data/ContrastImage.bmp");
+	SaveImage(BinImage, "image_data/BinImage.bmp");
+
+	SDL_FreeSurface(InputImage);
+}
+
+void testM()
+{
+	Matrix m1 = InitM(3, 2);
+	Matrix m2 = InitM(3, 2);
+
+	for(int i = 1; i < 7; i++)
+	{
+		PutPosM(m1, i-1, 2);
+		PutPosM(m2, i-1, rand() % 2);
+	}
+
+	Matrix t = TransM(m1);
+	Matrix a = AndM(m1, m2);
+	Matrix v = MultValM(m1, m2);
+	Matrix s = MultScalM(m1, 10);
+
+	DisplayM(m1);
+	DisplayM(m2);
+	DisplayM(v);
+	DisplayM(s);
+
+	FreeM(m1);
+	FreeM(m2);
+	FreeM(s);
+
+	DisplayM(t);
+	FreeM(t);
+
+	DisplayM(a);
+	FreeM(v);
+	FreeM(a);
+}
