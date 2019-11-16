833a480f8a6293830281266cb7c88be0eca462f8
william grolleau <william.grolleau@epita.fr>
2019-11-16 20:36:05 +0100

    fonction for char

--- testsegm.c
+++ testsegm.c
@@ -73,6 +73,73 @@ void testBin(char* path)
     DeleteL(lg);
 }
 
+void char_func(char* path)
+{
+    List* lg = paragraph_segm(path);
+    List * lp = lg;
+
+    /*
+    int p = 0;
+    int l = 0;
+    int w = 0;
+    int c = 0;
+    */
+    while(lp != NULL)
+    {
+        line_segm(lp);
+        List *ll = lp->child;
+        //SaveMat(lp, p, "para");
+        //p++;
+
+        while(ll != NULL)
+        {
+            word_segm(ll);
+            List *lw = ll->child;
+            //SaveMat(ll, l, "line");
+            //l++;
+
+            while(lw != NULL)
+            {
+                char_segm(lw);
+                List *lc = lw->child;
+                //SaveMat(lw, w, "word");
+                //w++;
+
+                while(lc != NULL)
+                {
+                    SaveMat(lc, c, "char");
+                    c++;
+
+                    /**
+                     * Liste de char du mot lc, les char sont dans le bon ordre
+                     * pour le mort abcde :
+                     * lc = a -> b -> c -> d -> e -> NULL
+                     * par contre pour lc, lw et ll c'est à l'envers, pour :
+                     * tu peux retirer les comms pour save les images dans le
+                     * doss image_data/rlsa pour voir dans quel ordre elles
+                     * sont.
+                     */
+                    lc = lc->next;
+                    //lc = NULL;
+                }
+
+                lw = lw->next;
+                //lw = NULL;
+            }
+
+            ll = ll->next;
+            //ll = NULL;
+        }
+
+        lp = lp->next;
+        //lp = NULL;
+    }
+
+    DeleteL(lg);
+
+}
+
+
 void SaveMat(List *l,  int i, char *n)
 {
     char *sint = Itoa(i);
