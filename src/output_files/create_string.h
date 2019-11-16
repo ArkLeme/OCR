7b326be3ee872932a0e9d3f7a7d1b73004f6b73e
william grolleau <william.grolleau@epita.fr>
2019-11-17 00:11:49 +0100

    add .h

--- src/output_files/create_string.h
+++ src/output_files/create_string.h
@@ -7,6 +7,12 @@ char* char_string(List *chr);
 
 char* word_string(List *word);
 
+char* line_segm(List *line);
+
+char* para_segm(List *para);
+
+char* text_segm(List *text);
+
 void write_files(char* path, List *l);
 
 #endif
