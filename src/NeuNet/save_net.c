67cda0d0dfac718b18929f3a0f67a5374cf5eeec
Jeanne MORIN <jeanne.morin@epita.fr>
2019-11-21 23:30:49 +0100

    savec net

---
+++ src/NeuNet/save_net.c
@@ -0,0 +1,42 @@
+#include "../matrix/matrix.h"
+#include "structNet.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "save_net.h"
+
+void SaveNeuNet(neuNet *n)
+{
+	FILE *p;
+	p = fopen("network", "w");
+	if(p == NULL)
+	{
+		//errx("File can't be created");
+	}
+	fprintf(p, "%i\n", n->nbLay);	
+	for(int i = 0;i < n->nbLay; i++)
+	{
+		fprintf(p, "%i\n", n->layers[i]->nbNeurons);
+	}
+	for(int i = 0; i < n->nbLay; i++)
+	{
+		layer* cL = n->layers[i];
+		for(int j = 0; j < cL->weights->size; j++)
+		{
+			fprintf(p,"%f\n", GetPosM(cL->weights, j));
+		}
+	}
+	fclose(p);
+}
+
+void LoadNeuNet()
+{
+	FILE *p = fopen("network", "r");
+	if(p == NULL)
+	{
+		//errx("File can't be created");
+	}
+	
+		
+}
+
