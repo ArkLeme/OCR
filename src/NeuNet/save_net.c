0cb5221954768f35802ad2d78daf9ac6a5d9956e
Pierre-Olivier Rey <pierre-olivier.rey@epita.fr>
2019-11-26 09:21:56 +0100

    comments to help Jeanne

--- src/NeuNet/save_net.c
+++ src/NeuNet/save_net.c
@@ -16,19 +16,21 @@ void SaveNeuNet(neuNet *n)
 	{
 		for(int i = 0;i < n->nbLay; i++)
 		{
-			fputc(n->layers[i]->nbNeurons,file);
+			fputc(n->layers[i]->nbNeurons,file);//ne fonctionne pas car un nombre != 
+			//un character
+			//pense à Itoa(int to string) qu'a fait william et la fonctin fprintf("%s\n", itoa); 
 			fputc(' ', file);
 		}
 		fputc('\n',file);
 			
-		for(int i = 0;i < n->nbLay; i++)
+		for(int i = 0;i < n->nbLay; i++) //premier layer n'a rien a save (pas de poids biais sur le premier)
 		{
 			layer *cL = n->layers[i];
 			printf("%i",i);
 			
 			printf("biases layer %i",i);
 			
-			if(cL->biases->size != fwrite(cL->biases, sizeof(double), cL->biases->size-1, file))
+			if(cL->biases->size != fwrite(cL->biases, sizeof(double), cL->biases->size-1, file)) //Je pense pas qu'il y ai besoin du  puisque tu écris le nb d'element
 				{printf("Pas write tous les élèements !!");}
 			else
 { 
@@ -36,7 +38,7 @@ void SaveNeuNet(neuNet *n)
 
 			printf("weights layer %i", i);
 			DisplayM(cL->weights);
-			fwrite(cL->weights, sizeof(double), cL->weights->size-1, file);
+			fwrite(cL->weights, sizeof(double), cL->weights->size-1, file);//idem
 			fputc('\n', file);}
 				
 			
@@ -71,7 +73,7 @@ neuNet *LoadNeuNet()
 
 		while(r)		
 		{
-			layers_nbNeurons[NbLay] = r;
+			layers_nbNeurons[NbLay] = r; //a changer avec double while ou autre fonction de lecture genre fgets
 			fgetc(file);
 			r = fgetc(file);
 			NbLay+=1;
@@ -80,14 +82,14 @@ neuNet *LoadNeuNet()
 		neuNet *network = init_network(layers_nbNeurons, NbLay);
 		
 		
-		for(int i = 0;i < network->nbLay; i++)
+		for(int i = 0;i < network->nbLay; i++) //du coup peut etre commencer à  si tu save pas le 1er layer
 		{
 			layer *cL = network->layers[i];
 
 			fread(cL->biases, sizeof(double), cL->biases->size, file);
 			fgetc(file);
 			fread(cL->weights, sizeof(double), cL->weights->size, file);
-			fgetc(file);		
+			fgetc(file); // ce for c'est grave clean		
 		
 		}
 	 	
