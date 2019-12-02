6e34d0fd02606674f08fdeae24de9f58629fe7ca
Alexandre LEMONNIER <alexandre.lemonnier@epita.fr>
2019-12-02 18:09:25 +0100

    Doc fixed <3

--- src/UI/callback.c
+++ src/UI/callback.c
@@ -447,35 +447,8 @@ void cb_about (GtkWidget *p_widget, gpointer user_data)
 
 void cb_doc(GtkWidget *p_widget,gpointer user_data)
 {
-	GtkWidget *test_dialog = NULL;
+	system("i3-sensible-terminal -e make doc &");
 
-	test_dialog = gtk_dialog_new_with_buttons 
-					("Continue ?",
-					GTK_WINDOW(docs.p_main_window),
-					GTK_DIALOG_MODAL,
-					"Yes",GTK_RESPONSE_YES,
-					"No",GTK_RESPONSE_NO,
-					NULL);
-	GtkWidget *label = gtk_label_new (NULL);
-	gtk_label_set_markup (GTK_LABEL (label), 
-	"<b> If you start Doc program,\n you will not be able to use the UI, continue ?</b>");
-	gtk_box_pack_start(GTK_BOX(gtk_dialog_get_content_area(
-						GTK_DIALOG(test_dialog))),
-						label, TRUE, TRUE, 5);
-	gtk_widget_show_all (test_dialog);
-	switch (gtk_dialog_run (GTK_DIALOG (test_dialog)))
-	{
-		case GTK_RESPONSE_YES:
-			gtk_widget_destroy (test_dialog);
-			gtk_widget_hide(docs.p_main_window);
-			system("xterm -e make doc");
-			gtk_widget_show_all(docs.p_main_window);
-			return;
-		case GTK_RESPONSE_NO:
-			gtk_widget_destroy (test_dialog);
-			return;
-		break;
-	}
 	/* Unused parameter */
 	(void) p_widget;
 	(void) user_data;
